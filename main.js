103b5d204f08235d22c944f31dd0c8dad3f886d1
javohir-site-developer <javohirjasurovich999@gmail.com>
2024-09-30 19:28:29 +0500

    javohir

--- main.js
+++ main.js
@@ -1,78 +1,79 @@
 //1
-// let acc = 0;
-// let arr = [1, 3, 7, 5];
-// const turn = arr.reduce((acc, item) => {
-//   return (arr = acc + item / item);
-// });
-// console.log(arr);
+let acc = 0;
+let arr = [1, 3, 7, 5];
+const turn = arr.reduce((acc, item) => {
+  return (arr = acc + item / item);
+});
+console.log(arr);
 
 //6
-// function findSmallestInt(arr) {
-//   return Math.min(...arr);
-// }
+function findSmallestInt(arr) {
+  return Math.min(...arr);
+}
 
-// console.log(findSmallestInt([78, 56, 232, 12, 8]));
+console.log(findSmallestInt([78, 56, 232, 12, 8]));
 
 //4
-// function firstNonConsecutive(hub) {
-//   for (let i = 0; i < hub.length; i++) {
-//     console.log(hub[i]);
-//   }
-// }
-// firstNonConsecutive([1, 2, 3, 4]);
+function firstNonConsecutive(hub) {
+  for (let i = 0; i < hub.length; i++) {
+    console.log(hub[i]);
+  }
+}
+firstNonConsecutive([1, 2, 3, 4]);
 
 //10
 
-// function between(d, c) {
-//   const javob = [];
-//   for (let i = d; i <= c; i++) {
-//     javob.push(i);
-//   }
-//   return javob;
-// }
-// const array = between(-4, 7);
-// console.log(array);
+function between(d, c) {
+  const javob = [];
+  for (let i = d; i <= c; i++) {
+    javob.push(i);
+  }
+  return javob;
+}
+const array = between(-4, 7);
+console.log(array);
 
 //9
 
-// function isTriangle(a, b, c) {
-//   if (a + c > b && a + b > c && b + c > a) {
-//     console.log("true");
-//   } else {
-//     console.log("false");
-//   }
-// }
+function isTriangle(a, b, c) {
+  if (a + c > b && a + b > c && b + c > a) {
+    console.log("true");
+  } else {
+    console.log("false");
+  }
+}
 
-// isTriangle(3, 4, 5);
+isTriangle(3, 4, 5);
 
 //2
 
-// function cutVowel(when) {
-//   const green = ["aeiou"];
-//   return when.replace(green, " ");
-// }
+function cutVowel(text) {
+  return text
+    .split("")
+    .filter((char) => !"aeiouAEIOU".includes(char))
+    .join("");
+}
 
-// const red = "Javohirning kundaligi";
-// const javob = cutVowel(red);
-// console.log(javob);
+console.log(cutVowel("This website is for losers LOL!"));
 
 //5
 
-// function arrNumbers(array) {
-//   const goog = {};
-//   for (const son of array) {
-//     goog[son] = (goog[son] || 0) + 1;
-//   }
+function arrNumbers(array) {
+  const goog = {};
 
-//   for (const son in goog) {
-//     if (goog[son] % 2 !== 0) {
-//       return Number(son);
-//     }
-//   }
+  for (const son of array) {
+    goog[son] = (goog[son] || 0) + 1;
+  }
 
-//   return null;
-// }
+  for (const son in goog) {
+    if (goog[son] % 2 !== 0) {
+      return Number(son);
+    }
+  }
 
-// const son = [1, 2, 2, 5, 5];
-// const til = arrNumbers(son);
-// console.log(til);
+  return null;
+}
+
+const son = [1, 2, 2, 5, 5];
+const til = arrNumbers(son);
+console.log(til);
