2d0eac8f84cef0d448f2a47206775f32abb490f1
javohir-site-developer <javohirjasurovich999@gmail.com>
2024-09-30 17:21:44 +0500

    javohirniki

---
+++ main.js
@@ -0,0 +1,22 @@
+// //1
+// let acc = 0;
+// let arr = [1, 3, 7, 5];
+// const turn = arr.reduce((acc, item) => {
+//   return (arr = acc + item / item);
+// });
+// console.log(arr);
+
+// //6
+// function findSmallestInt(arr) {
+//   return Math.min(...arr);
+// }
+
+// console.log(findSmallestInt([78, 56, 232, 12, 8]));
+
+//4
+function firstNonConsecutive(hub) {
+  for (let i = 0; i < hub.length; i++) {
+    console.log(hub[i]);
+  }
+}
+firstNonConsecutive([1, 2, 3, 4]);
